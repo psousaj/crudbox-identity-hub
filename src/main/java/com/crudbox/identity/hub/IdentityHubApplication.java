5c90524269a3a0e84c7252a8d3beb1a103ec84eb
José Pinheiro <josepsousa2012@gmail.com>
2025-04-01 21:41:02 -0300

    First Commit

---
+++ src/main/java/com/crudbox/identity/hub/IdentityHubApplication.java
@@ -0,0 +1,13 @@
+package com.crudbox.identity.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crudbox/identity/hub/IdentityHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudbox.identity.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
